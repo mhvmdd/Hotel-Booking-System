6943d7c8e41ba4e60d20a60964ba780449b893ba
Lenovo <mohammeduni585@gmail.com>
2025-04-25 07:27:19 +0300

    Added the main project files

---
+++ src/main/java/org/advcomprog/hotelbooking/hotelbookingsystem/HotelBookingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.advcomprog.hotelbooking.hotelbookingsystem;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class HotelBookingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
